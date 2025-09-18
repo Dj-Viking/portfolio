9d778a82d47ae452ad2e44d6cf4a9002fb62927c
Dj-Viking <anders.swedishviking@gmail.com>
2025-09-18 10:58:38 -0400

    getting page started and dev environment

---
+++ app.mjs
@@ -0,0 +1,11 @@
+const body = document.body;
+export function main(
+	appModule
+) {
+	initStyles();
+}
+
+function initStyles() {
+	console.log("init styles");
+	body.style.backgroundColor = "white";
+}
---
+++ index.mjs
@@ -0,0 +1,23 @@
+import { WS_PORT } from "./common.mjs";
+
+(async () => {
+    
+    const ws = new WebSocket(`ws://localhost:${WS_PORT}`)
+
+    let app = await import("./app.mjs");
+    
+    ws.addEventListener('message', async (event) => {
+        if (event.data.includes("mjs")) {
+            const cachebust = "?v=" + (Date.now().toString())
+            app = await import("./app.mjs" + cachebust);
+            app.main(
+                app,
+            );
+        }
+    });
+
+    app.main(
+        app,
+    );
+
+})();
